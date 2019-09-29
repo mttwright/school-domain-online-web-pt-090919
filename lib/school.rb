00146f99dc9b21c113e5d09f14602f41dd886b52
virtual-frame-7789 <mttwright@gmail.com>
2019-09-29 23:04:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+  
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.key?(grade) == false
+      @roster[grade] = []
+      @roster[grade].push(name)
+    else
+      @roster[grade].push(name)
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |key, student_array|
+      @roster[key] = student_array.sort
+    end
+  end
+  
+end
