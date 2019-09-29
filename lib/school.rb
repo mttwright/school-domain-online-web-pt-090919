8c6bcd15385880bfb6deee26d3e1f526419877ad
virtual-frame-7789 <mttwright@gmail.com>
2019-09-29 22:30:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,9 @@ class School
     @roster = {}
   end
   
-  def add_student([name], grade)
-    @roster[grade] = [name]
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade].push(name)
   end
   
 end
